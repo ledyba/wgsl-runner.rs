58dff6440cccb1911d044b0fe6075f9eba8b9383
Kaede Fujisaki <kaede@hexe.net>
2022-04-24 20:42:53 +0900

    INIT

---
+++ src/main.rs
@@ -0,0 +1,101 @@
+use std::future::Future;
+use std::pin::Pin;
+use log::info;
+
+pub mod png;
+pub mod render;
+
+// https://github.com/gfx-rs/wgpu-rs/tree/master/examples/capture
+
+fn main() -> anyhow::Result<()> {
+  env_logger::init_from_env(env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"));
+  let rt =
+    tokio::runtime::Builder::new_current_thread()
+      .enable_all()
+      .build()?;
+  let width = 512;
+  let height = 512;
+  rt.block_on::<Pin<Box<dyn Future<Output=anyhow::Result<()>>>>>(Box::pin(async {
+    let instance = wgpu::Instance::new(wgpu::Backends::all());
+    info!("{} adapters found.", instance.enumerate_adapters(wgpu::Backends::all()).collect::<Vec<_>>().len());
+    let adapter =
+      instance.request_adapter(&wgpu::RequestAdapterOptions::default()).await
+        .ok_or(anyhow::Error::msg("Adapter not found."))?;
+    let (device, queue) = adapter
+      .request_device(
+        &wgpu::DeviceDescriptor {
+          label: None,
+          features: wgpu::Features::empty(),
+          limits: wgpu::Limits::default(),
+        },
+        None,
+      )
+      .await?;
+    let buffer_dimensions = render::BufferDimensions::new(width, height);
+    let output_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+      label: None,
+      size: (buffer_dimensions.padded_bytes_per_row * buffer_dimensions.height) as u64,
+      usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
+      mapped_at_creation: false,
+    });
+    let texture_extent = wgpu::Extent3d {
+      width: buffer_dimensions.width as u32,
+      height: buffer_dimensions.height as u32,
+      depth_or_array_layers: 1,
+    };
+    // The render pipeline renders data into this texture
+    let texture = device.create_texture(&wgpu::TextureDescriptor {
+      size: texture_extent,
+      mip_level_count: 1,
+      sample_count: 1,
+      dimension: wgpu::TextureDimension::D2,
+      format: wgpu::TextureFormat::Rgba8UnormSrgb,
+      usage: wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::COPY_SRC,
+      label: None,
+    });
+    // Set the background to be red
+    let command_buffer = {
+      let mut encoder =
+        device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+      encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+        label: None,
+        color_attachments: &[wgpu::RenderPassColorAttachment {
+          view: &texture.create_view(&wgpu::TextureViewDescriptor::default()),
+          resolve_target: None,
+          ops: wgpu::Operations {
+            load: wgpu::LoadOp::Clear(wgpu::Color::RED),
+            store: true,
+          },
+        }],
+        depth_stencil_attachment: None,
+      });
+
+      // Copy the data from the texture to the buffer
+      encoder.copy_texture_to_buffer(
+        wgpu::ImageCopyTexture {
+          texture: &texture,
+          mip_level: 0,
+          origin: wgpu::Origin3d::ZERO,
+          aspect: Default::default()
+        },
+        wgpu::ImageCopyBuffer {
+          buffer: &output_buffer,
+          layout: wgpu::ImageDataLayout {
+            offset: 0,
+            bytes_per_row: Some(
+              std::num::NonZeroU32::new(buffer_dimensions.padded_bytes_per_row as u32)
+                .unwrap(),
+            ),
+            rows_per_image: None,
+          },
+        },
+        texture_extent,
+      );
+      encoder.finish()
+    };
+    queue.submit(Some(command_buffer));
+    crate::png::save("out.png", device, output_buffer, &buffer_dimensions).await;
+    Ok(())
+  }))?;
+  Ok(())
+}
---
+++ src/png.rs
@@ -0,0 +1,50 @@
+use std::pin::Pin;
+use std::io::Write;
+use tokio::io::AsyncWriteExt;
+
+pub async fn save(
+  png_output_path: &str,
+  device: wgpu::Device,
+  output_buffer: wgpu::Buffer,
+  buffer_dimensions: &crate::render::BufferDimensions,
+) {
+  // Note that we're not calling `.await` here.
+  let buffer_slice = output_buffer.slice(..);
+  let buffer_future = buffer_slice.map_async(wgpu::MapMode::Read);
+
+  // Poll the device in a blocking manner so that our future resolves.
+  // In an actual application, `device.poll(...)` should
+  // be called in an event loop or on another thread.
+  device.poll(wgpu::Maintain::Wait);
+  buffer_future.await.unwrap();
+
+  let padded_buffer = buffer_slice.get_mapped_range();
+
+  let mut png_encoder = png::Encoder::new(
+    std::fs::File::create(png_output_path).unwrap(),
+    buffer_dimensions.width as u32,
+    buffer_dimensions.height as u32,
+  );
+  png_encoder.set_depth(png::BitDepth::Eight);
+  png_encoder.set_color(png::ColorType::Rgba);
+  let mut png_writer = png_encoder
+    .write_header()
+    .unwrap()
+    .into_stream_writer_with_size(buffer_dimensions.unpadded_bytes_per_row)
+    .unwrap();
+
+  // from the padded_buffer we write just the unpadded bytes into the image
+  for chunk in padded_buffer.chunks(buffer_dimensions.padded_bytes_per_row) {
+    png_writer
+      .write_all(&chunk[..buffer_dimensions.unpadded_bytes_per_row])
+      .unwrap();
+  }
+  png_writer.finish().unwrap();
+
+  // With the current interface, we have to make sure all mapped views are
+  // dropped before we unmap the buffer.
+  drop(padded_buffer);
+
+  output_buffer.unmap();
+
+}
